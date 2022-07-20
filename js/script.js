5bc4bf6eff362a3d696c79b571da03f759fa9cc9
oliver-zyn <olivermayer15@gmail.com>
2022-07-19 23:13:32 -0300

    atualizacoes header

--- js/script.js
+++ js/script.js
@@ -1,9 +1,39 @@
+const navbar = document.getElementById("navbar");
+
+const links = navbar.getElementsByClassName("link");
+
+const mobileButton = document.querySelector('#navbar--btnMobile')
+
+for (let link of links) {
+  link.addEventListener("click", function(e) {
+    let current = document.getElementsByClassName("active");
+
+    if (current.length > 0) {
+      current[0].classList.remove("active");
+    }
+
+    console.log(e.target);
+
+    e.target.classList.add("active");
+  });
+}
+
+function menu(){
+  navbar.classList.toggle('activeN')
+  mobileButton.classList.toggle('activeM')
+}
+
+window.onscroll = () => {
+  navbar.classList.remove('activeN')
+  mobileButton.classList.toggle('activeM')
+}
+
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
+    let reveals = document.querySelectorAll(".reveal");
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = reveals[i].getBoundingClientRect().top;
+      let elementVisible = 150;
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
       } else {
